fad045ec80016f456732da5be9b4e5fabaa956fe
Aamirkhan121 <aamir73690@gmail.com>
2024-08-07 11:27:23 +0530

    backend

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./error/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename=fileURLToPath(import.meta.url);
+// const __dirname=path.dirname(__filename)
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+// console.log(__dirname)
+
+// Middleware
+app.use(cors({
+  origin: 'https://66b306cb60cf90691f00b329--radiant-shortbread-9610f7.netlify.app',
+  credentials: true,
+}));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/reservation", reservationRouter);
+
+// app.use(express.static(path.join(__dirname,'/client/dist')))
+// app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'/client/dist/index.html')))
+
+dbConnection();
+
+// Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+app.use(errorMiddleware);
+
+// Example route
+app.get("/home", (req, res) => {
+  res.status(200).send("Home page");
+});
+
+export default app;
